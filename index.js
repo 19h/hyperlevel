3d97171eb27fcc18f76ee2b7ea617a236f37f3ab
Kenan Sulayman <kenan@sly.mn>
2013-08-29 20:10:56 +0200

    Lets put this baby online.

---
+++ index.js
@@ -0,0 +1,21 @@
+var     levelup = require('levelup'),
+	ldH     = require("leveldown-hyper"),
+     	ldh_f   = function (location) { return new ldH(location) }
+
+module.exports = function LevelUP (location, options, callback) {
+	if ( options instanceof Function ) {
+		callback = options
+		options  = { db: ldh_f }
+	}
+
+	// Can't be done more efficiently;
+	// Object.create(null) wouldn't have
+	// Object as constructor nor would it instanciate it
+	if ( typeof options === "object" )
+		options["db"] = ldh_f;
+
+	if ( !options )
+		options = { db: ldh_f };
+
+	return levelup ( location, options, callback )
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+var assert = require('assert')
+
+// :)
+assert(require('./') !== require('levelup'))
+
+// :)
+console.log('All good! level !== levelup')
